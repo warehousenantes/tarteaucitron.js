4b03c8d3cb09b54bcee1abf75f8dea824be6c7d8
Simon <simon@daig.re>
2022-09-18 22:13:25 +0200

    fix gtag

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -25,15 +25,17 @@ tarteaucitron.services.multiplegtag = {
         "use strict";
         window.dataLayer = window.dataLayer || [];
 
-        if (tarteaucitron.user.multiplegtagUa !== undefined) {
-            tarteaucitron.user.multiplegtagUa.forEach(function (ua) {
-                tarteaucitron.addScript('https://www.googletagmanager.com/gtag/js?id=' + ua, '', function () {
-                    window.gtag = function gtag() { dataLayer.push(arguments); }
-                    gtag('js', new Date());
-                    var additional_config_info = (timeExpire !== undefined) ? {'anonymize_ip': true, 'cookie_expires': timeExpire / 1000} : {'anonymize_ip': true};
-                    gtag('config', ua, additional_config_info);
-                });
+        tarteaucitron.user.multiplegtagUa.forEach(function (ua) {
+            tarteaucitron.addScript('https://www.googletagmanager.com/gtag/js?id=' + ua, '', function () {
+                window.gtag = function gtag() { dataLayer.push(arguments); }
+                gtag('js', new Date());
+                var additional_config_info = (timeExpire !== undefined) ? {'anonymize_ip': true, 'cookie_expires': timeExpire / 1000} : {'anonymize_ip': true};
+                gtag('config', ua, additional_config_info);
             });
+        });
+
+        if (typeof tarteaucitron.user.multiplegtagMore === "function") {
+            tarteaucitron.user.multiplegtagMore();
         }
     }
 };
@@ -48,6 +50,7 @@ tarteaucitron.services.facebookpixel = {
     "cookies": ['datr', 'fr', 'reg_ext_ref', 'reg_fb_gate', 'reg_fb_ref', 'sb', 'wd', 'x-src', '_fbp'],
     "js": function () {
         "use strict";
+
         var n;
         if (window.fbq) return;
         n = window.fbq = function () { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments) };
@@ -76,34 +79,32 @@ tarteaucitron.services.tiktok = {
     cookies: [],
     js: function () {
         "use strict";
-        if (null !== tarteaucitron.user.tiktokId) {
-            !function (w, d, t) {
-                w.TiktokAnalyticsObject = t;
-                var ttq = w[t] = w[t] || [];
-                ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie"], ttq.setAndDefer = function (t, e) {
-                    t[e] = function () {
-                        t.push([e].concat(Array.prototype.slice.call(arguments, 0)))
-                    }
-                };
-                for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
-                ttq.instance = function (t) {
-                    for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(e, ttq.methods[n]);
-                    return e
-                }, ttq.load = function (e, n) {
-                    var i = "https://analytics.tiktok.com/i18n/pixel/events.js";
-                    ttq._i = ttq._i || {}, ttq._i[e] = [], ttq._i[e]._u = i, ttq._t = ttq._t || {}, ttq._t[e] = +new Date, ttq._o = ttq._o || {}, ttq._o[e] = n || {};
-                    var o = document.createElement("script");
-                    o.type = "text/javascript", o.async = !0, o.src = i + "?sdkid=" + e + "&lib=" + t;
-                    var a = document.getElementsByTagName("script")[0];
-                    a.parentNode.insertBefore(o, a)
-                };
-                ttq.load(tarteaucitron.user.tiktokId);
-                ttq.page();
-            }(window, document, 'ttq');
+        !function (w, d, t) {
+            w.TiktokAnalyticsObject = t;
+            var ttq = w[t] = w[t] || [];
+            ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie"], ttq.setAndDefer = function (t, e) {
+                t[e] = function () {
+                    t.push([e].concat(Array.prototype.slice.call(arguments, 0)))
+                }
+            };
+            for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
+            ttq.instance = function (t) {
+                for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(e, ttq.methods[n]);
+                return e
+            }, ttq.load = function (e, n) {
+                var i = "https://analytics.tiktok.com/i18n/pixel/events.js";
+                ttq._i = ttq._i || {}, ttq._i[e] = [], ttq._i[e]._u = i, ttq._t = ttq._t || {}, ttq._t[e] = +new Date, ttq._o = ttq._o || {}, ttq._o[e] = n || {};
+                var o = document.createElement("script");
+                o.type = "text/javascript", o.async = !0, o.src = i + "?sdkid=" + e + "&lib=" + t;
+                var a = document.getElementsByTagName("script")[0];
+                a.parentNode.insertBefore(o, a)
+            };
+            ttq.load(tarteaucitron.user.tiktokId);
+            ttq.page();
+        }(window, document, 'ttq');
 
-            if (typeof tarteaucitron.user.tiktokMore === "function") {
-                tarteaucitron.user.tiktokMore();
-            }
+        if (typeof tarteaucitron.user.tiktokMore === "function") {
+            tarteaucitron.user.tiktokMore();
         }
     },
 };
